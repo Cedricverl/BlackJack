183f012b961112c166c0d593cb4a40476d8d8c9a
Cedricverl <56162986+Cedricverl@users.noreply.github.com>
2021-01-27 21:20:30 +0100

    ready for big sim!

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ import numpy as np
 
 deck = get_deck() * 8
 random.shuffle(deck)
-# donedeck = []
+donedeck = []
 # deck = [Card(3), Card(3), Card(3), Card(3), Card(3), Card(3), Card(7), Card(4), Card(5), Card(2), Card(6), Card(3), Card(4), Card(6), Card(3), Card(3), Card(1), Card(1), Card(4), Card(1), Card(1)]
 # print("deck: ", deck)
 bet = 5
@@ -34,7 +34,7 @@ def getaction(hand, dealervalue):
         if (1 < othercard < 7 and 4 < dealervalue < 7) or (3 < othercard < 7 and dealervalue == 4) or (
                 othercard == 6 and dealervalue == 3):
             if len(hand.getCards()) == 2:
-                return "hit"  # if allowed, otherwise hit
+                return "double"  # if allowed, otherwise hit
             else:
                 return "hit"
         if (othercard == 8 and dealervalue == 6) or (othercard == 7 and 1 < dealervalue < 7):
@@ -56,12 +56,6 @@ def getaction(hand, dealervalue):
         return "stand"
     else:
         return "hit"
-
-
-def merge(a, b):
-    c = a.copy()
-    c.extend(b)
-    return c
 
 
 def playhand(hand, dealercards):
@@ -94,7 +88,8 @@ def playhand(hand, dealercards):
             # print("play second pair")
             b = playhand(hand2, dealercards)
             # print("a =",a,"b =",b)
-            return merge(a, b)
+            a.extend(b)
+            return a
         elif action == "surrender":
             hand.setSurrender()
             return [hand]
@@ -159,25 +154,27 @@ if __name__ == "__main__":
     # playround()
     # print(bankroll)
 
-    for j in range(3):
-        print("starting 100 000 sets of 100 games...")
-        bankrollist = []
-        for i in range(10):
-            print(i)
-            for k in range(10000):
-                bankroll = 100
-                for p in range(100):
-                    playround()
-                bankrollist.append(bankroll)
-                random.shuffle(deck)
-        print("after 100000 sets of 100 games you ended op on avg with", np.mean(bankrollist), "euro")
-        print("with stdev of", sqrt(np.var(bankrollist)))
+    # for j in range(3):
+    #     print("starting 100 000 sets of 100 games...")
+    #     bankrollist = []
+    #     for i in range(10):
+    #         print(i)
+    #         for k in range(100):
+    #             bankroll = 100
+    #             for p in range(100):
+    #                 playround()
+    #             bankrollist.append(bankroll)
+    #             random.shuffle(deck)
+    #     print("after 100000 sets of 100 games you ended op on avg with", np.mean(bankrollist), "euro")
+    #     print("with stdev of", sqrt(np.var(bankrollist)))
 
-    # result = []
-    # for i in range(100000):
-    #     bankroll = 100
-    #     playround()
-    #     random.shuffle(deck)
-    #     result.append(bankroll)
-    # print("mean:", np.mean(result))
-    # print("stdev:", sqrt(np.var(result)))
+    result = []
+    for k in range(100):
+        print(k)
+        for i in range(5000000):
+            bankroll = 100
+            playround()
+            random.shuffle(deck)
+            result.append(bankroll)
+    print("mean:", np.mean(result))
+    print("stdev:", sqrt(np.var(result)))
